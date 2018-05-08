21cf1f2f6dc618044301c57e2ecb71b8430ce707
Rémi Delfosse <remi.delfosse@eisti.fr>
2018-05-08 16:58:41 +0200

    test binding firebase databese to listView Worksites in welcome screen

--- app/src/main/java/com/example/rvadam/pfe/WelcomeListWorkSites/WelcomeListWorkSite.java
+++ app/src/main/java/com/example/rvadam/pfe/WelcomeListWorkSites/WelcomeListWorkSite.java
@@ -2,7 +2,6 @@ package com.example.rvadam.pfe.WelcomeListWorkSites;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.ListView;
 
 import com.example.rvadam.pfe.R;
@@ -13,8 +12,6 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 /**
  * Created by rdelfoss on 04/05/2018.
@@ -45,67 +42,17 @@ public class WelcomeListWorkSite extends AppCompatActivity {
         mListView = (ListView) findViewById(R.id.listViewWorkSites);
 
         mDatabase.addValueEventListener(new ValueEventListener() {
-                                            @Override
-                                            public void onDataChange(DataSnapshot dataSnapshot) {
-                                                WorkSite workSite = dataSnapshot.getValue(WorkSite.class);
-                                                lWorksites.add(workSite);
-                                                adapter.notifyDataSetChanged();
-                                            }
-
-                                            @Override
-                                            public void onCancelled(DatabaseError databaseError) {
-
-                                            }
-                                        }
-
-
-        // Add value event listener to the post
-
-        ValueEventListener worksitesListener = new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-
-                List<WorkSite> lWorksites = readFromFirebaseDB(dataSnapshot);
-                Log.i(TAG, lWorksites.toString());
-
-
-                mListView.setAdapter(adapter);
-
-                /*mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                        WorkSite workSite = lWorksites.get(position);
-                        WorkSiteActivity.setInnerWorkSite(workSite);
-
-                        Intent intent = new Intent(WelcomeListWorkSite.this, WorkSiteActivity.class);
-                        startActivity(intent);
-                    }
-                });*/
-
+                WorkSite workSite = dataSnapshot.getValue(WorkSite.class);
+                lWorksites.add(workSite);
+                adapter.notifyDataSetChanged();
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+
             }
-        };
-        myRef.addValueEventListener(worksitesListener);
-
-    }
-
-    public List<WorkSite> readFromFirebaseDB(DataSnapshot dataSnapshot) {
-        List<WorkSite> worksitesList = new ArrayList<WorkSite>();
-        Iterable<DataSnapshot> dataList = dataSnapshot.getChildren();
-
-        Iterator<DataSnapshot> it = dataList.iterator();
-
-        while (it.hasNext()) {
-            WorkSite rt = it.next().getValue(WorkSite.class);
-            worksitesList.add(rt);
-        }
-
-        return worksitesList;
+        });
     }
 }
